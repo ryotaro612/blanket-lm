2abf9a5c89b13deb24cda09b9ad83a67746ab77a
Ryotaro <ryotaro612.ooo@gmail.com>
2025-05-11 17:28:55 +0900

    feat(markdown): implement posts retrieval and CSV writing functionality
    - Added methods to retrieve markdown posts from the specified directory.
    - Implemented CSV writing for the extracted posts, including support for English and Japanese content.

--- py/src/blanketlm/markdown.py
+++ py/src/blanketlm/markdown.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+import sys
+import csv
 import argparse
 from os import path
 import os
@@ -12,10 +15,48 @@ class Website:
         self.basedir = basedir
 
     @property
-    def files(self):
-        dir = path.join(self.basedir, "content", "posts")
-        ls = os.listdir(dir)
-        print(ls)
+    def posts_dir(self):
+        """
+        Returns the path to the posts directory.
+
+        """
+        return path.join(self.basedir, "content", "posts")
+
+    @property
+    def filenames(self):
+        """
+        Returns absolute paths to all files in the content/posts directory.
+
+        """
+        return [f for f in os.listdir(self.posts_dir) if f.endswith(".md")]
+
+    @property
+    def posts(self):
+        """
+        Returns a content written in Japanese or English.
+
+        """
+        filenames = self.filenames
+
+        posts: dict[str, dict[str, str | None]] = defaultdict(
+            lambda: {"en": None, "ja": None}
+        )
+
+        for filename in filenames:
+            filepath = path.join(self.posts_dir, filename)
+            if filename.endswith("en.md"):
+                name = filename[:-5]
+                lang = "en"
+            elif filename.endswith("ja.md"):
+                name = filename[:-5]
+                lang = "ja"
+            else:
+                name = filename[:-3]
+                lang = "ja"
+
+            posts[name][lang] = filepath
+
+        return posts
 
 
 def parse(args: list[str]) -> argparse.Namespace:
@@ -25,10 +66,19 @@ def parse(args: list[str]) -> argparse.Namespace:
     return parser.parse_args(args)
 
 
-def _markdown(args: list[str]):
+def write_posts(dest_file, posts: dict[str, dict[str, str | None]]):
+    with open(dest_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["name", "en", "ja"])
+        for name, post in posts.items():
+            writer.writerow([name, post["en"], post["ja"]])
+
+
+def find_markdown(args: list[str]):
     namespace = parse(args)
     website = Website(namespace.website)
+    write_posts(namespace.output, website.posts)
 
-    website.files
 
-    print("doge")
+def _markdown():
+    find_markdown(sys.argv[1:])
