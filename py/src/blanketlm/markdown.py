634d5e6b6e1940a36b87e79622a251baf695e7d0
Ryotaro <ryotaro612.ooo@gmail.com>
2025-05-11 16:49:49 +0900

    feat(markdown): enhance markdown processing with website class and argument parsing
    - Introduced a Website class to manage file retrieval from specified directories.
    - Added argument parsing for command-line interface to improve usability.

--- py/src/blanketlm/markdown.py
+++ py/src/blanketlm/markdown.py
@@ -1,3 +1,34 @@
-def _markdown():
+import argparse
+from os import path
+import os
+import dataclasses
+
+
+@dataclasses.dataclass
+class Website:
+    """ """
+
+    def __init__(self, basedir: str):
+        self.basedir = basedir
+
+    @property
+    def files(self):
+        dir = path.join(self.basedir, "content", "posts")
+        ls = os.listdir(dir)
+        print(ls)
+
+
+def parse(args: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("website")
+    parser.add_argument("output")
+    return parser.parse_args(args)
+
+
+def _markdown(args: list[str]):
+    namespace = parse(args)
+    website = Website(namespace.website)
+
+    website.files
 
     print("doge")
